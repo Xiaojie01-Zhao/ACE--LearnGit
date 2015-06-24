17a8db7948b9975e1f3c979d3770ff69c625c0b4
Zhao <763764553@qq.com>
2015-06-24 10:20:10 +0800

    提交了fsdfsf

---
+++ LearnGit/sfgdgdf.h
@@ -0,0 +1,13 @@
+//
+//  sfgdgdf.h
+//  LearnGit
+//
+//  Created by lanouhn on 15/6/24.
+//  Copyright (c) 2015年 ZhaoXiaojie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sfgdgdf : UIView
+
+@end
---
+++ LearnGit/sfgdgdf.m
@@ -0,0 +1,21 @@
+//
+//  sfgdgdf.m
+//  LearnGit
+//
+//  Created by lanouhn on 15/6/24.
+//  Copyright (c) 2015年 ZhaoXiaojie. All rights reserved.
+//
+
+#import "sfgdgdf.h"
+
+@implementation sfgdgdf
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
